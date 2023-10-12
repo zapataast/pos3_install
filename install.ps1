962c2fa0111bc3e0dbd07cb6a288f76bcfba23ef
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2023-10-12 10:01:46 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,2 @@
 mkdir "C:\sqlite\"
-Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/blob/main/sqlite3.exe -OutFile  "C:\sqlite\sqlite3.exe"
-Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/blob/main/sqldiff.exe -OutFile  "C:\sqlite\sqldiff.exe"
-Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/blob/main/sqlite3_analyzer.exe -OutFile  "C:\sqlite\sqlite3_analyzer"
+Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/archive/refs/heads/main.zip -OutFile  "C:\sqlite\sqlite.zip"
