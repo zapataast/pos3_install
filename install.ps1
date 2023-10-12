5d86195512f83190670c35a070030ac2d3784273
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2023-10-12 10:03:29 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
 mkdir "C:\sqlite\"
 Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/archive/refs/heads/main.zip -OutFile  "C:\sqlite\sqlite.zip"
+Unzip "C:\sqlite\sqlite.zip" "C:\sqlite\"
