ab1111ede66905ad4988a312639ee307208a0568
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2023-10-12 10:11:15 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
 mkdir "C:\sqlite\"
 Invoke-WebRequest -Uri https://github.com/zapataast/pos3_install/archive/refs/heads/main.zip -OutFile  "C:\sqlite\sqlite.zip"
 Unzip "C:\sqlite\sqlite.zip" "C:\sqlite\"
+[Environment]::SetEnvironmentVariable("PATH", "$env:PATH;C:\sqlite\pos3_install-main", [System.EnvironmentVariableTarget]::Machine)
